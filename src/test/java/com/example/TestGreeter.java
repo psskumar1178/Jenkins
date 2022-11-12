d39cc603afa71b76a2dd21c496a8043c70e611ac
Subrahmanya Kumar Peesapati Sri <P7166200@ness.com>
2022-11-12 10:34:52 +0530

    Added new test case in TestGreeter file

--- src/test/java/com/example/TestGreeter.java
+++ src/test/java/com/example/TestGreeter.java
@@ -33,6 +33,13 @@ public class TestGreeter {
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
+  @Test
+  public void greetShouldIncludeGreetingPhrase1() {
+    String someone = "World";
+
+    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
+  }
+
 
     @Test
   public void greetShouldIncludeGreetingMessage() {
